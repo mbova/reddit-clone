8801f7142f1f40bee67b1be3ed8e0b15f8c46670
Michael Bova <bovavob@gmail.com>
2014-07-16 21:05:01 -0500

    adding comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,26 @@
+class CommentsController < ApplicationController
+
+  def create
+    @topic = Topic.find(params[:topic_id])
+    @post = @topic.posts.find(params[:post_id])
+    @comments = @post.comments
+
+    @comment = current_user.comments.build(comment_params)
+    @comment.post = @post
+    authorize @comment
+
+    if @comment.save
+      flash[:notice] = "Comment was saved."
+      redirect_to [@topic, @post]
+    else
+      flash[:error] = "There was an error saving the comment. Please try again."
+      render 'posts/show'
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment)
+  end
+end
